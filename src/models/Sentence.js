31dec850a644ad94eec8210e2930d51d4beee123
Brise <gabriel_ns@live.com>
2019-12-30 23:07:35 -0300

    getScore after vote save

--- src/models/Sentence.js
+++ src/models/Sentence.js
@@ -11,41 +11,4 @@ const SentenceSchema = new mongoose.Schema({
   isApproved: Boolean
 });
 
-// Count votes and check if is approved or rejected
-
-// SentenceSchema.statics.getScore = async function() {
-//   try {
-//     await this.model("Votes").findById(sentenceId)
-//   } catch (err) {
-
-//   }
-// }
-
-// Static method to get avg rating and save
-// SentenceSchema.statics.getRating = async function(sentenceId) {
-//   const obj = await this.aggregate([
-//     {
-//       $match: { bootcamp: bootcampId }
-//     },
-//     {
-//       $group: {
-//         _id: "$sentence",
-//         averageRating: { $avg: "$rating" }
-//       }
-//     }
-//   ]);
-//   try {
-//     await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
-//       averageRating: obj[0].averageRating
-//     });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-// Call getAverageCost after save
-// ReviewSchema.post("save", function() {
-//   this.constructor.getAverageRating(this.bootcamp);
-// });
-
 module.exports = mongoose.model("Sentence", SentenceSchema);
--- src/models/Vote.js
+++ src/models/Vote.js
@@ -12,4 +12,32 @@ const VoteSchema = new mongoose.Schema({
   }
 });
 
+// Count votes and check if is approved or rejected
+
+VoteSchema.statics.getScore = async function(sentenceId) {
+  const obj = await this.aggregate([
+    {
+      $match: { sentence: sentenceId }
+    },
+    {
+      $group: {
+        _id: "$sentence",
+        score: { $sum: { $cond: ["$is_positive", 1, -1] } }
+      }
+    }
+  ]);
+  try {
+    await this.model("Sentence").findByIdAndUpdate(sentenceId, {
+      score: obj[0].score
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Call getAverageCost after save
+VoteSchema.post("save", function() {
+  this.constructor.getScore(this.sentence);
+});
+
 module.exports = mongoose.model("Vote", VoteSchema);
