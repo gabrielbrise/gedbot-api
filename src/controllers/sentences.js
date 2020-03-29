2fafb5550b834a8ea83acbcc8141e070dc9242c7
Brise <gabriel_ns@live.com>
2020-03-29 04:00:38 -0300

    sentences now return user and vote context

--- src/controllers/sentences.js
+++ src/controllers/sentences.js
@@ -1,11 +1,43 @@
 const asyncHandler = require("../middleware/async");
 const Sentence = require("../models/Sentence");
+const User = require("../models/User");
+const Vote = require("../models/Vote");
+const crypto = require("crypto");
 
 // @desc      Get all sentences
 // @route     GET /api/v1/sentences
 // @access    Public
 exports.getSentences = asyncHandler(async (req, res, next) => {
-  const sentences = await Sentence.find();
+  let sentences = await Sentence.find();
+  const votes = await Vote.find();
+  const ip = crypto
+    .createHash("sha256")
+    .update(req.ip)
+    .digest("hex");
+
+  let user = await User.findOne({ ip });
+
+  if (!user) {
+    user = await User.create({ ip });
+  }
+
+  const userVotes = votes.filter(vote => vote.user === user._id);
+  const userVotesSentencesIds = userVotes.map(vote => vote.sentence);
+  const userVotesSentencesisPositive = userVotes.map(vote => vote.isPositive);
+
+  if (userVotes) {
+    sentences = sentences.map((sentence, index) => {
+      if (userVotesSentencesIds.includes(sentence._id)) {
+        return {
+          ...sentences,
+          isVoted: true,
+          isPositive: userVotesSentencesisPositive[index]
+        };
+      }
+      return sentence;
+    });
+  }
+
   return res.status(200).json({
     success: true,
     count: sentences.length,
